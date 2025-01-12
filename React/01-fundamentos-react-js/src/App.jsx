b17ebab8234e811c47cb1a38ae37fb8e3a9a6a6d
RaulMS03 <raulsmascarenhas@gmail.com>
2025-01-12 20:52:50 -0300

    feat: add header, sidebar, styles and assets (configs and dependencies)

--- React/01-fundamentos-react-js/src/App.jsx
+++ React/01-fundamentos-react-js/src/App.jsx
@@ -1,7 +1,23 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
+
 function App() {
   return (
-    <h1>HELLO WORLD</h1>
-  )
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post></Post>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ React/01-fundamentos-react-js/src/components/Post.jsx
@@ -0,0 +1,51 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/RaulMS03.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Raul Mascarenhas</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="12 de Janeiro às 19:08" dateTime="2025-01-12 19:08:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto </a> <a href="">#nlw </a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
